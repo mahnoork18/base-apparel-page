956cc62df0b0263b931b9d76e40e2eb77f9812d6
Mahnoor Khan <68016846+mahnoork18@users.noreply.github.com>
2021-10-15 01:38:40 +0500

    commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+const form = document.getElementById('form')
+const email = document.getElementById('email')
+
+// listening the submit event on form
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const emailValue = email.value
+
+    if(emailValue === '' || !validateEmail(emailValue)) {
+        form.className = 'form error' 
+    } else {
+        form.className = 'form success'
+    }
+})
+// function with REGEX for email validation...
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(String(email).toLowerCase());
+}
